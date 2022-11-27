e6e42f4edea1eff6b4c7cf0b2465eeabff2e3b1c
Travis Nickles <ryochan7@protonmail.com>
2022-11-27 11:45:39 -0600

    Enable Touch Click as a mappable button

--- DS4MapperTest/DS4Library/DS4Mapper.cs
+++ DS4MapperTest/DS4Library/DS4Mapper.cs
@@ -60,7 +60,7 @@ namespace DS4MapperTest.DS4Library
                 new InputBindingMeta("Share", "Share", InputBindingMeta.InputControlType.Button),
                 new InputBindingMeta("Options", "Options", InputBindingMeta.InputControlType.Button),
                 new InputBindingMeta("PS", "PS", InputBindingMeta.InputControlType.Button),
-                //new InputBindingMeta("Touch Click", "Touch Click", InputBindingMeta.InputControlType.Button),
+                new InputBindingMeta("Touch Click", "Touch Click", InputBindingMeta.InputControlType.Button),
                 new InputBindingMeta("LS", "Left Stick", InputBindingMeta.InputControlType.Stick),
                 new InputBindingMeta("RS", "Right Stick", InputBindingMeta.InputControlType.Stick),
                 new InputBindingMeta("DPad", "DPad", InputBindingMeta.InputControlType.DPad),
@@ -345,6 +345,14 @@ namespace DS4MapperTest.DS4Library
                 if (currentMapperState.PS || currentMapperState.PS != previousMapperState.PS)
                 {
                     tempBtnAct.Prepare(this, currentMapperState.PS);
+                }
+
+                if (tempBtnAct.active) tempBtnAct.Event(this);
+
+                tempBtnAct = currentLayer.buttonActionDict["Touch Click"];
+                if (currentMapperState.TouchClickButton || currentMapperState.TouchClickButton != previousMapperState.TouchClickButton)
+                {
+                    tempBtnAct.Prepare(this, currentMapperState.TouchClickButton);
                 }
 
                 if (tempBtnAct.active) tempBtnAct.Event(this);
