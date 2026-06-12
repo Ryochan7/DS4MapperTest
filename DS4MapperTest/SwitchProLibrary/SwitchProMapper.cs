8984e0313b14293422e55d63db400028660ac057
Travis Nickles <ryochan7@protonmail.com>
2026-06-12 15:38:26 -0500

    Passthrough gyro data from Switch Pro controller

--- DS4MapperTest/SwitchProLibrary/SwitchProMapper.cs
+++ DS4MapperTest/SwitchProLibrary/SwitchProMapper.cs
@@ -375,7 +375,7 @@ namespace DS4MapperTest.SwitchProLibrary
                 // Skip if duration is less than 10 ms
                 //if (currentMapperState.timeElapsed > 0.01)
                 {
-                    GyroEventFrame mouseFrame = new GyroEventFrame
+                    GyroEventFrame gyroFrame = new GyroEventFrame
                     {
                         GyroYaw = currentMapperState.Motion.GyroYaw,
                         GyroPitch = currentMapperState.Motion.GyroPitch,
@@ -394,7 +394,8 @@ namespace DS4MapperTest.SwitchProLibrary
                         elapsedReference = device.BaseElapsedReference * 3,
                     };
 
-                    gyroAct.Prepare(this, ref mouseFrame);
+                    PopulateStateGyro(ref gyroFrame);
+                    gyroAct.Prepare(this, ref gyroFrame);
                     if (gyroAct.active)
                     {
                         gyroAct.Event(this);
